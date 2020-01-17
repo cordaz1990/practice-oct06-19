62e9a4c92d05f8c9052687ebd1fdf208a975638c
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-16 19:42:23 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,9 @@
-words = ["whale", "mango", "orange"]
-new_word = ["tape"]
-print()
+observations_to_birds_list = {}
+for bird, observations in bird_to_observations.items():
+  if observations in observations_to_birds_list:
+    observations_to_birds_list[observations].append(bird)
+  else:
+    observations_to_birds_list[observations] = [bird]
+    
+ observations_to_birds_list
+{1:['nother fulmar', 'snow goose'], 2:['long-tailed jaeger'], 5:['canada goose']}
